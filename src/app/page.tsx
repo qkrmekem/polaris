663ad470f9c9c7a80733abdaae5bb2fe812e516f
qkrmekem <magicofclown@naver.com>
2026-02-04 01:57:50 +0900

    project setting

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+const X = () => {
+  return (
+    <div className="font-medium">
+      <Button>
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default X;
