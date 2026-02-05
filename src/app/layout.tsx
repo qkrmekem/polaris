07bb62ba99c8358caf2999061d0bb87f8d7c1306
qkrmekem <magicofclown@naver.com>
2026-02-05 23:21:32 +0900

    Add Clerk authentication integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 
 import { ThemeProvider } from "@/components/them-provider";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 import "./globals.css";
 
@@ -27,6 +35,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
+    <ClerkProvider>
     <html lang="en" suppressHydrationWarning >
       <body
         className={`${inter.variable} ${plexMono.variable} antialiased`}
@@ -37,10 +47,25 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <header>
+            <SignedOut>
+            <SignInButton/>
+            <SignUpButton>
+              <button className="bg-rose-500 text-white p-2 rounded">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
+          </header>
+          
           {children}
         </ThemeProvider>
         
       </body>
     </html>
+    </ClerkProvider>
   );
 }
